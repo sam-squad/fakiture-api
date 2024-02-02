0d24aca8f2e538b53c53a05fe0f86c2f24d6b657
Salohyy212 <zerambaysalohy@gmail.com>
2024-02-02 21:19:52 +0300

    init : spring boot

---
+++ src/main/java/com/example/fakitureapi/FakitureApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakitureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakitureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakitureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fakitureapi/FakitureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakitureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakitureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
