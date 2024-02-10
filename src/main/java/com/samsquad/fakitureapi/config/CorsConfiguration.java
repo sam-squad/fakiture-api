a0fefa6574bb9588be45b111ff5d9ac14643d789
Salohyy212 <zerambaysalohy@gmail.com>
2024-02-10 20:39:29 +0300

    feat: implements basic authentification

---
+++ src/main/java/com/samsquad/fakitureapi/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.samsquad.fakitureapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // A ajouter ici l'url de front
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+}
